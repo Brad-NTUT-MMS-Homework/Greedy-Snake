bd66f5348c0cf2a12b87d2a2b0d358519184ee9a
lrnbrad <lrnbrad@gmail.com>
2023-12-27 23:16:07 +0800

    feat(main.c): complete logic flow

--- Greedy-Snake/source/main.c
+++ Greedy-Snake/source/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#include "data.h"
+#include "draw.h"
+#include "input.h"
+#include "logic.h"
+#include "sleep.h"
+
+
 int main() {
-	printf("startup project! welcome");
+	Snake snake = { RIGHT, false, 1, 0 };
+	BaseSetup baseSetup = { 20, 50, {20 / 2, 50 / 2} };
+
+	while (!snake.gameover) {
+		Draw(&snake, &baseSetup);
+		Input(&snake);
+		Logic(&snake, &baseSetup);
+		sleep_ms(1000);
+	}
 }
