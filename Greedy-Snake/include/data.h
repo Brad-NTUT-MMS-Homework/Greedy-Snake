ddfae7a5f4b4dd4f533da75d4186076df9da5148
lrnbrad <lrnbrad@gmail.com>
2023-12-27 22:21:56 +0800

    Feat(data.h): 增加資料型別

---
+++ Greedy-Snake/include/data.h
@@ -0,0 +1,28 @@
+#ifndef DATA_H_
+#define DATA_H_
+
+#include <stdbool.h>
+
+typedef enum tDirection {
+	STOP = 0, LEFT, RIGHT, UP, DOWN
+} Direction;
+
+typedef struct sfruit {
+	int xPos;
+	int yPos;
+} Fruit;
+
+typedef struct sSnake {
+	Direction direction;
+	bool gameover;
+	int length;
+	int score;
+} Snake;
+
+typedef struct sBaseSetup {
+	int width;
+	int height;
+	Fruit fruit;
+};
+
+#endif
