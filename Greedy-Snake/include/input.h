c0bfde703976411c8144516fa542d4ef6650358e
lrnbrad <lrnbrad@gmail.com>
2023-12-27 23:02:07 +0800

    chore(input): add entry for input function

---
+++ Greedy-Snake/include/input.h
@@ -0,0 +1,8 @@
+#ifndef INPUT_H_
+#define INPUT_H_
+
+#include "data.h" // Include necessary headers
+
+void Input(Snake *snake);
+
+#endif // INPUT_H_
---
+++ Greedy-Snake/source/input.c
@@ -0,0 +1,7 @@
+#include <conio.h>
+#include "Data.h"
+
+void Input(Snake *snake) {
+	// Implementation of the Input function
+	// Use the snake structure for processing input
+}
