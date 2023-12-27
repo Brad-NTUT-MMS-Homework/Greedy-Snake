3e136b47844448a7eae52747312cadd70a26912d
lrnbrad <lrnbrad@gmail.com>
2023-12-27 23:02:33 +0800

    chore(logic): add entry for logic function

---
+++ Greedy-Snake/include/logic.h
@@ -0,0 +1,8 @@
+#ifndef LOGIC_H_
+#define LOGIC_H_
+
+#include "data.h" // Include necessary headers
+
+void Logic(Snake *snake, BaseSetup *baseSetup);
+
+#endif // LOGIC_H_
---
+++ Greedy-Snake/source/logic.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "Data.h"
+
+void Logic(Snake *snake, BaseSetup *baseSetup) {
+	// Implementation of the Logic function
+	// Use snake and baseSetup structures for game logic
+}
