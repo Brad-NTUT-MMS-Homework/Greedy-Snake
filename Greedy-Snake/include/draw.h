84aa15615c668e4bc97f9f21428b2d3084d49c96
lrnbrad <lrnbrad@gmail.com>
2023-12-27 23:01:32 +0800

    chore(draw): add entry for draw function

---
+++ Greedy-Snake/include/draw.h
@@ -0,0 +1,8 @@
+#ifndef DRAW_H_
+#define DRAW_H_
+
+#include "data.h" // Include necessary headers
+
+void Draw(Snake *snake, BaseSetup *baseSetup);
+
+#endif // DRAW_H_
---
+++ Greedy-Snake/source/draw.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "Data.h"
+
+void Draw(Snake *snake, BaseSetup *baseSetup) {
+	// Implementation of the Draw function
+	// Use snake and baseSetup structures for drawing
+}
