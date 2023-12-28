1675aa6a625bf23608e5cc0d25a222c14e0c2239
DSSETU <a0985254424@gmail.com>
2023-12-28 14:44:39 +0800

    feat(logic): 完成程式邏輯

--- Greedy-Snake/source/logic.c
+++ Greedy-Snake/source/logic.c
@@ -4,4 +4,18 @@
 void Logic(Snake *snake, BaseSetup *baseSetup) {
 	// Implementation of the Logic function
 	// Use snake and baseSetup structures for game logic
+	int prevX = snake->pos.xPos;
+	int prevY = snake->pos.yPos;
+	int prev2X, prev2Y;
+	snake->pos.xPos = prevX;
+	snake->pos.yPos = prevY;
+
+	for (int i = 1; i < snake->length; i++) {
+		prev2X = snake->pos.xPos;
+		prev2Y = snake->pos.yPos;
+		snake->pos.xPos = prevX;
+		snake->pos.yPos = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
 }
