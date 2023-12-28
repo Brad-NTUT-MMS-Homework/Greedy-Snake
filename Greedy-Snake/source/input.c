7c4e0c18e9e5296044fcd74e5b9814b20ce9f858
DSSETU <a0985254424@gmail.com>
2023-12-28 14:45:31 +0800

    feat(input): 完成程式操作輸入

--- Greedy-Snake/source/input.c
+++ Greedy-Snake/source/input.c
@@ -4,4 +4,44 @@
 void Input(Snake *snake) {
 	// Implementation of the Input function
 	// Use the snake structure for processing input
+	switch (_getch()) {
+	case 'a':
+		snake->direction == LEFT;
+		break;
+	case 'd':
+		snake->direction == RIGHT;
+		break;
+	case 'w':
+		snake->direction == UP;
+		break;
+	case 's':
+		snake->direction == DOWN;
+		break;
+	case '\x1b[D':
+		snake->direction == LEFT;
+		break;
+	case '\x1b[C':
+		snake->direction == RIGHT;
+		break;
+	case '\x1b[A':
+		snake->direction == UP;
+		break;
+	case '\x1b[B':
+		snake->direction == DOWN;
+		break;
+	case 'x':
+		snake->gameover = 1;
+
+	if (snake->direction == UP) {
+		snake->pos.yPos--;
+	}
+	if (snake->direction == DOWN) {
+		snake->pos.yPos++;
+	}
+	if (snake->direction == LEFT) {
+		snake->pos.xPos--;
+	}
+	if (snake->direction == RIGHT) {
+		snake->pos.xPos++;
+	}
 }
