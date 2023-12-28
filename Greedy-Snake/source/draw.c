e55f2613dc794291fd6940667d293c89f7311f45
lrnbrad <lrnbrad@gmail.com>
2023-12-28 12:04:47 +0800

    draft

--- Greedy-Snake/source/draw.c
+++ Greedy-Snake/source/draw.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+
+#include "../include/draw.h"
 #include "../include/data.h"
 
 void Draw(Snake *snake, BaseSetup *baseSetup) {
 	// Implementation of the Draw function
 	// Use snake and baseSetup structures for drawing
-}
+	system("cls");
+	for (int i = 0; i < baseSetup->width + 2; i++)
+		printf("#");
+	printf("\n");
+
+	for (int i = 0; i < baseSetup->height; i++) {
+		for (int j = 0; j < baseSetup->width; j++) {
+			if (j == 0)
+				printf("#");
+			if (i == snake->pos.yPos && j == snake->pos.xPos)
+				printf("O");
+			else if (i == baseSetup->fruit.yPos && j == baseSetup->fruit.xPos)
+				printf("F");
+			else {
+				int printTail = 0;
+				for (int k = 1; k < snake->length; k++) {
+					if (i == snake->pos.yPos[k] && j == snake->pos.xPos[k]) {
+						printf("o");
+						printTail = 1;
+					}
+				}
+				if (!printTail)
+					printf(" ");
+			}
+			if (j == baseSetup->width - 1)
+				printf("#");
+		}
+		printf("\n");
+	}
