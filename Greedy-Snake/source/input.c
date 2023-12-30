d04ad8af0cef0b2127d1dd2fd246d9e51e9cb581
DSSETU <a0985254424@gmail.com>
2023-12-30 15:54:55 +0800

    fix(input): 更新因為Windows ASCII不同而造成的無法控制，並移動邏輯至logic.c

--- Greedy-Snake/source/input.c
+++ Greedy-Snake/source/input.c
@@ -4,44 +4,33 @@
 void Input(Snake *snake) {
 	// Implementation of the Input function
 	// Use the snake structure for processing input
-	switch (_getch()) {
-	case 'a':
-	case '\x1b[D':
-		snake->direction == LEFT;
-		break;
-	case 'd':
-	case '\x1b[C':
-		snake->direction == RIGHT;
-		break;
-	case 'w':
-	case '\x1b[A':
-		snake->direction == UP;
-		break;
-	case 's':
-	case '\x1b[B':
-		snake->direction == DOWN;
-		break;
-	case 'x':
-		snake->gameover = 1;
-		break;
-	default:
-		break;
-
-	}
-	for (int i = 0; i < snake->length; i++) {
-
-		if (snake->direction == UP) {
-			snake->pos[i].yPos--;
-		}
-		if (snake->direction == DOWN) {
-			snake->pos[i].yPos++;
-		}
-		if (snake->direction == LEFT) {
-			snake->pos[i].xPos--;
-		}
-		if (snake->direction == RIGHT) {
-			snake->pos[i].xPos++;
+	if (generalKbhit()) {
+		switch (_getch()) {
+		case 'a':
+		case '\x1b[D':
+		case 75:
+			snake->direction = LEFT;
+			break;
+		case 'd':
+		case '\x1b[C':
+		case 77:
+			snake->direction = RIGHT;
+			break;
+		case 'w':
+		case '\x1b[A':
+		case 72:
+			snake->direction = UP;
+			break;
+		case 's':
+		case '\x1b[B':
+		case 80:
+			snake->direction = DOWN;
+			break;
+		case 'x':
+			snake->gameover = 1;
+			break;
+		default:
+			break;
 		}
 	}
-
 }
