f71afd4442f25131007f34922c78b0022456e45c
lrnbrad <lrnbrad@gmail.com>
2024-01-02 20:56:57 +0800

    chore(input): 修正重複引用

--- Greedy-Snake/source/input.c
+++ Greedy-Snake/source/input.c
@@ -1,37 +1,37 @@
 #include <conio.h>
+
 #include "../include/data.h"
-#include "input.h"
 
 void Input(Snake *snake) {
-	// Implementation of the Input function
-	// Use the snake structure for processing input
-	if (generalKbhit()) {
-		switch (_getch()) {
-		case 'a':
-		case '\x1b[D':
-		case 75:
-			snake->direction = LEFT;
-			break;
-		case 'd':
-		case '\x1b[C':
-		case 77:
-			snake->direction = RIGHT;
-			break;
-		case 'w':
-		case '\x1b[A':
-		case 72:
-			snake->direction = UP;
-			break;
-		case 's':
-		case '\x1b[B':
-		case 80:
-			snake->direction = DOWN;
-			break;
-		case 'x':
-			snake->gameover = 1;
-			break;
-		default:
-			break;
-		}
-	}
+    // Implementation of the Input function
+    // Use the snake structure for processing input
+    if (generalKbhit()) {
+        switch (_getch()) {
+            case 'a':
+            case '\x1b[D':
+            case 75:
+                snake->direction = LEFT;
+                break;
+            case 'd':
+            case '\x1b[C':
+            case 77:
+                snake->direction = RIGHT;
+                break;
+            case 'w':
+            case '\x1b[A':
+            case 72:
+                snake->direction = UP;
+                break;
+            case 's':
+            case '\x1b[B':
+            case 80:
+                snake->direction = DOWN;
+                break;
+            case 'x':
+                snake->gameover = 1;
+                break;
+            default:
+                break;
+        }
+    }
 }
